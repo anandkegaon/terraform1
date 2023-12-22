fe67e0becf13cde72d56d8e3eeb849f597c2f4df
Anand Kegaon <94635568+anandkegaon@users.noreply.github.com>
2023-12-22 15:32:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,8 @@ provider "aws" {
 resource "aws_instance" "server" {
 
     ami = "ami-079db87dc4c10ac91"
-
+   // (give the downloded key name from aws ) //
+   key_name = "1213"
     instance_type = "t2.micro"
     tags = {
         "Name"="sevrer"
