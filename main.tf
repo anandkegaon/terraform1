edd6c68693fa3b36bcf3af99c393d58aefc63d88
Anand Kegaon <94635568+anandkegaon@users.noreply.github.com>
2023-12-22 14:36:53 +0530

    ec2instance

---
+++ main.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+
+    region = "us-east-1"
+  
+}
+
+resource "aws_instance" "server" {
+
+    ami = "ami-079db87dc4c10ac91"
+
+    instance_type = "t2.micro"
+    tags = {
+        "Name"="sevrer"
+    }
+
+  
+}
